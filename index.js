b5e4685122ca491e18626fc55505a8f0177b120a
p.watthanavarangkul <p.watthanvarangkul@campus.tu-berlin.de>
2019-07-08 14:34:58 +0200

    handle email confirmation

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express')
+const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 const port = 8000
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+var auth = {
+  type: 'oauth2',
+  user: 'awtproject.signup@gmail.com',
+  clientId: '145650667844-ru36qg84otlcl1vbp8h19frbcc06p16s.apps.googleusercontent.com',
+  clientSecret: '6sgIugmL6mmgSFfN1d2-JMoV',
+  refreshToken: '1/BzHIPxqUILPqA6p3H7hTgk7DNF7zDzOfRr_R7h0VXmBD_E__Pw0CJEsLxz0LZORn',
+};
+
+app.use(bodyParser.json()) // handle json data
+app.use(bodyParser.urlencoded({ extended: true })) // handle URL-encoded data
+
+//app.use(express.urlencoded());
+//app.use(express.multipart());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
@@ -34,6 +48,33 @@ app.get('/students', db.getStudents)
 app.get('/group_pref', db.getGroupPref)
 
 app.post('/group', db.createGroup)
+
+app.post('/send_email', function(req, res){
+  response = {
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message
+  }
+  var mailOptions = {
+    from: req.body.name,
+    to: req.body.email,
+    subject: 'AWT Project signup confirmation: ',
+    text: req.body.message,
+    html: 'Message from: ' + req.body.name + '<br></br> Email: ' +  req.body.email + '<br></br> Message: ' + req.body.message,
+  };
+  var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: auth,
+  });
+  transporter.sendMail(mailOptions, (err, res) => {
+      if (err) {
+          return console.log(err);
+      } else {
+          console.log(JSON.stringify(res));
+      }
+  });
+})
+  
 
 
 
